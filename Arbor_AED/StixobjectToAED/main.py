740565ff145bb8418a4402a657c705b3deb0c945
zhri <zhri.plt@hotmail.com>
2022-04-02 06:15:14 -0400

    Stix/taxii

---
+++ Arbor_AED/StixobjectToAED/main.py
@@ -0,0 +1,67 @@
+import requests,urllib3,random,Get_Collection
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+from datetime import datetime
+time=datetime.now()
+
+class StixObject:
+
+    def __init__(self,ip,id,user,passwd):
+        self.ip=ip
+        self.id=id
+        self.user=user
+        self.passwd=passwd
+        self.url=f"https://{self.ip}/taxii/root/collections/{self.id}/objects/"
+
+
+    def AdBlackList(self,values):
+        session=requests.session()
+        session.auth=(f"{self.user}",f"{self.passwd}")
+        aut=session.post(self.url,verify=False)
+        headers={'Content-type': 'application/vnd.oasis.stix+json; version=2.0'}
+        for i in values:
+            randomA = random.randint(0, 1000)
+            randomB = random.randint(0, 1000)
+            json = {
+                "id": "bundle--14d13cfb-cf39-4d5" + str(randomB) + "d-926e-a0b2892c6b" + str(randomA) + "a",
+                "objects": [
+                    {
+                        "id": "indicator--754ed1e6-e1fa-49f" + str(randomA) + "-92c4-4710bb7ac3d" + str(randomB),
+                        "labels": [
+                            "bidirectional"
+                        ],
+                        "name": "Brandefense",
+                        "pattern": "[ipv4-addr:value = '{}']".format(i),
+                        "type": "indicator",
+                        "valid_from": str(time)
+                    }
+                ],
+                "spec_version": "2.0",
+                "type": "bundle"
+            }
+            response = session.post(self.url, headers=headers, json=json, verify=False)
+        session.close()
+        print("Ip (IPs) added")
+
+    def DeleteIp(self,values):
+        pass
+
+
+    def GetBlackList(self):
+        with open("IpList", "r") as file:
+            Lines = file.readlines()
+            file.close()
+        self.AdBlackList(Lines)
+
+while True:
+    try:
+        Get_Collection
+        ip=Get_Collection.ip
+        id=Get_Collection.id
+        user=Get_Collection.user
+        passwd=Get_Collection.passwd
+        s=StixObject(ip,id,user,passwd)
+        s.GetBlackList()
+        break
+    except:
+        print("hata")
+        break
---
+++ Arbor_AED/StixobjectToAED/readme.txt
@@ -0,0 +1,8 @@
+-> For this action you need the stix/taxii licence
+
+-> user name and password is stix/taxii client name and password
+        *Administration->stix/taxii->add a client
+-> you need collection for your stix object
+        *Administration->stix/taxii->collection->Add a collection
+-> for more information, user your arbor aed help documantain by "stix/taxii"
+-> for more information about stix/taxii visit "https://oasis-open.github.io/cti-documentation/"
