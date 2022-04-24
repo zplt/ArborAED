877ced93ebd8b044b868595b8fcfc3e1ab768afe
zplt <zhri.plt@hotmail.com>
2022-04-24 23:23:38 +0300

    OTF

---
+++ Arbor_AED/DeleteBlackListFromOutBound/DeleteBlackList_OTF.py
@@ -0,0 +1,22 @@
+import requests,urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+ip=input("Arbor_Ip: ").strip(" ")
+token=str(input("Arbor_Token: ")).strip(" ")
+
+url=f"https://{ip}/api/aed/v2/otf/blacklisted-hosts"
+param="/?hostAddress="
+
+list=[]
+with open("DeletList","r") as file:
+    list=file.readlines()
+    file.close()
+
+session = requests.session()
+headers = {'X-Arbux-APIToken':f'{token}'}
+
+for i in list:
+    i=str(i).strip("\n")
+    full_path=(url+param+ f"{i}")
+    r=session.delete(full_path, headers=headers, verify=False )
+    print(r.status_code)
+    print(r.text)
